3dd8803a07576964043b47884cad15a125c863d1
Julien_P <jparis.py@gmail.com>
2020-06-29 18:41:57 +0200

    some changes but mainly modifying from within nuxt project

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import axios from 'axios'
 
+function getSwaggerJson (url) {
+  try {
+    let response = axios.get(url)
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 class APILib {
   constructor (options) {
+    console.log('>>> APILib > options : ', options)
     this.options = options
     const protocol = this.options.protocol || 'https'
     this.swaggerURL = `${protocol}://${this.options.swaggerUrl}`
@@ -9,16 +19,23 @@ class APILib {
     this.apiServerURL = `${protocol}://${this.options.apiDomain}${this.apiVersion}`
 
     this.swaggerObject = undefined
-    axios.get(this.swaggerURL)
-      .then(res => {
-        this.swaggerObject = JSON.parse(res)
-      })
-    console.log('>>> APILib > this.swaggerObject : \n', this.swaggerObject)
+    this.swaggerPaths = undefined
+
+    let getJson = getSwaggerJson(this.swaggerURL)
+    .then(res => {
+      this.swaggerObject = res.data
+      this.swaggerPaths = res.data.paths
+      return res
+    })
+    Promise.all([getJson])
+    .then( res => {
+      console.log('>>> APILib > Promise > this.swaggerObject :', this.swaggerObject)
+      return
+    })
   }
 
   _get (path, params) {
     params = params || {}
-    // TODO: handle errors
     return axios.get(`${this.apiServerURL}/${path}`, { params: params })
       .then(res => {
         return res.body
@@ -41,6 +58,13 @@ class APILib {
       })
   }
 
+  getSwagerObject () {
+    return this.swaggerObject
+  }
+  getSwagerPaths () {
+    return this.swaggerPaths
+  }
+
   getDatasets () {
     return this._get('datasets/')
   }
@@ -50,4 +74,5 @@ class APILib {
   }
 }
 
-module.export.APILib = APILib
+// module.export.APILib = APILib
+export default APILib
