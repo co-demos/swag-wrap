f7b39731d149a91a11ea579b27623d90c4baa3d4
Julien_P <jparis.py@gmail.com>
2020-07-01 16:08:59 +0200

    updates

--- README.md
+++ README.md
@@ -16,14 +16,19 @@ npm install @julpy/swag-wrap
 npm install @julpy/swag-wrap@0.0.1-beta.15
 ```
 
+--------
+## Main dependencie(s)
+
+- `swagger-client` : [github](https://github.com/swagger-api/swagger-js) - [documentation](https://github.com/swagger-api/swagger-js#readme) - [npm](https://www.npmjs.com/package/swagger-client)
+
+
 -------
-## Usage
+## Usage as vue plugin
 
 #### in your vue app's `.env` file
 ```env
 VUE_APP_SWAGGER_URL=https://www.data.gouv.fr/api/1/swagger.json
 VUE_APP_API_KEY=my-precious-api-personal-key
-VUE_APP_API_KEY_LOCATION=headers.auth
 ```
 
 #### in your vue app's `main.js` file
@@ -36,7 +41,6 @@ const swagWrapOptions = {
   // get back your swagger json url and other options here
   swaggerUrl: process.env.VUE_APP_SWAGGER_URL,
   apiKey: process.env.VUE_APP_API_KEY,
-  apiKeyLocation: process.env.VUE_APP_API_KEY_LOCATION,
   separator: '.'
 }
 
@@ -69,12 +73,11 @@ Vue.use(APIcli, options)
 export default {
   data () {
     return {
-      // define the path of your endpoint(s)
-      // both ways (paths as array or string) works
+      // declare your endpoint's operationId
       // this part is relative to your swagger specs
-      // note the order is important
-      endpointPathArray: ['datasets', 'list_datasets'],
-      endpointPathString: 'datasets.list_datasets',
+      operationId_get_list: 'list_datasets',
+      operationId_get_one: 'get_dataset',
+      operationId_put_one: 'update_dataset',
 
       // the data you want to load and then display
       datasets: undefined
@@ -84,8 +87,9 @@ export default {
     // get your swag-wrap client instance
     const API = this.$APIcli
 
+    //// GET endpoint without parameters
     // request data from the desired path
-    API._request(this.endpointPathString).then(
+    API._request(this.operationId_get_list).then(
       results => {
         // ... do something with the result
         this.datasets = results.body
@@ -105,12 +109,63 @@ export default {
       },
       reason => console.error('failed on api call: ' + reason)
     )
+
+    //// GET endpoint with parameters
+    // prepare parameters for this endpoint
+    const paramsOne = {
+      dataset: 'my-dataset-id',
+    }
+    // request data from the desired path
+    API._request(this.operationId_get_one, paramsOne).then(
+      results => {
+        // ... do something with the result
+        this.datasets = results.body
+      },
+      reason => console.error('failed on api call: ' + reason)
+    )
+
+    //// PUT endpoint with parameters
+    // prepare parameters for this endpoint
+    const paramsOnePut = {
+      dataset: 'my-dataset-id',
+      payload: {
+        title: 'my-new-title'
+      }
+    }
+    // PUT/POST data from the desired path
+    API._request(this.operationId_put_one, paramsOnePut).then(
+      results => {
+        // ... do something with the result
+        this.datasets = results.body
+      },
+      reason => console.error('failed on api call: ' + reason)
+    )
+
   }
 }
 </script>
 ```
 
---------
-## Main dependencie(s)
 
-- `swagger-client` : [github](https://github.com/swagger-api/swagger-js) - [documentation](https://github.com/swagger-api/swagger-js#readme) - [npm](https://www.npmjs.com/package/swagger-client)
+-------
+## Usage as plugin
+
+```js
+import { SwagCli } from '@julpy/swag-wrap'
+
+// initiate the api client from the swagger json
+const swagWrapOptions = {
+  swaggerUrl: 'https://www.data.gouv.fr/api/1/swagger.json',
+  apiKey: 'my-precious-api-personal-key',
+  separator: '.'
+}
+const mySwaggerClient = new SwagCli(options)
+
+// use your client to request the api
+// simple `get` example
+const operationId = 'list_datasets'
+const myRequest = mySwaggerClient(endpointPath).then( results => {
+  console.log(results)
+  // ... do something with the results
+})
+```
--- index.js
+++ index.js
@@ -1,24 +1,53 @@
 import SwaggerClient from 'swagger-client'
 
-const resolvePath = (path, obj = self, separator = '.') => {
-  const properties = Array.isArray(path) ? path : path.split(separator)
-  return properties.reduce((prev, curr) => prev && prev[curr], obj)
-}
+// const resolvePath = (path, obj = self, separator = '.') => {
+//   const properties = Array.isArray(path) ? path : path.split(separator)
+//   return properties.reduce((prev, curr) => prev && prev[curr], obj)
+// }
 
 class SwagCli {
   constructor (options) {
+    // this.options = options
+
     // retrieve spec from options
     this.spec = options.swaggerUrl
     console.log('>>> SwagCli > init > this.spec : ', this.spec)
 
     // retrieve API key options
     this.apiKey = options.apiKey
+    this.bearerAuth = options.bearerAuth
+    this.userName = options.userName
+    this.userPassword = options.userPassword
+    this.token = options.token
 
     // retrieve separator (optional)
     this.tagsSeparator = options.separator ? options.separator : '.'
 
     // create swagger client from spec
-    this.cli = new SwaggerClient({ url: this.spec })
+    let specAndAuth = { url: this.spec }
+
+    // add auth to spec if necessary
+    if (this.apiKey) {
+      specAndAuth.authorization = { ApiKey: { value: this.apiKey } }
+    } else if (this.bearerAuth) {
+      specAndAuth.authorization = { BearerAuth: { value: this.bearerAuth } }
+    } else if (this.userPassword) {
+      specAndAuth.authorization = { BasicAuth: { username: this.userName, password: this.userPassword } }
+    } else if (this.token) {
+      specAndAuth.authorization = { oAuth2: { token: { access_token: this.token } } }
+    }
+
+    // append interceptor
+    const requestInterceptor = this._requestInterceptor
+    specAndAuth = { ...specAndAuth, requestInterceptor }
+
+    // activate CORS or not
+    if (options.activateCORS) {
+      SwaggerClient.http.withCredentials = true
+    }
+
+    // instantiate client
+    this.cli = new SwaggerClient(specAndAuth)
 
     // set up security definitions
     this.security = undefined
@@ -27,16 +56,24 @@ class SwagCli {
 
   _setSecurity () {
     // cf : https://github.com/swagger-api/swagger-js/blob/HEAD/docs/usage/http-client.md
-    // console.log('>>> SwagCli > _setSecurity ...')
+    console.log('>>> SwagCli > _setSecurity ...')
     return this.cli.then(
       client => {
-        // console.log('>>> SwagCli > _setSecurity >> client.spec.securityDefinitions : ', client.spec.securityDefinitions)
+        console.log('>>> SwagCli > _setSecurity >> client.spec.securityDefinitions : ', client.spec.securityDefinitions)
         this.security = client.spec.securityDefinitions
       }
     )
   }
 
-  _request (pathTagList, data) {
+  _requestInterceptor (request) {
+    console.log('>>> SwagCli > _requestInterceptor > request :', request)
+    if (request.loadSpec) {
+      console.log('>>> SwagCli > _requestInterceptor > request.loadSpec :', request.loadSpec)
+    }
+    return request
+  }
+
+  _request (pathTagList, params) {
     // main request function
     // arg :: pathTagList : array|string of tags corresponding to your endpoint's path
     // note : the order is important
@@ -45,10 +82,25 @@ class SwagCli {
       client => {
         // get endpoint by resolving endpoint's path in client.apis
         console.log('>>> SwagCli > _request >> client : ', client)
-        console.log('>>> SwagCli > _request >> this.security : ', this.security)
-        const endpoint = resolvePath(pathTagList, client.apis, this.tagsSeparator)
-        // console.log('>>> SwagCli > _request >> endpoint : ', endpoint)
-        return endpoint(data)
+
+        if (this.security && this.apiKey) {
+          console.log('>>> SwagCli > _request >> this.security : ', this.security)
+          console.log('>>> SwagCli > _request >> this.apiKey : ', this.apiKey)
+        }
+
+        // build endpoint
+        // const endpoint = resolvePath(pathTagList, client.apis, this.tagsSeparator)
+
+        const _requestInterceptor = this._requestInterceptor
+        const endpoint = client.execute({
+          operationId: pathTagList,
+          parameters: params,
+          _requestInterceptor
+        })
+
+        console.log('>>> SwagCli > _request >> endpoint : ', endpoint)
+        // return endpoint(params)
+        return endpoint
       },
       reason => console.error('>>> SwagCli > _request >> failed to load the spec: ' + reason)
     )
